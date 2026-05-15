a609be9433ab38b035df8191dca74393030412b0
주형진 <hj06869@naver.com>
2026-05-15 17:07:30 +0900

    5/15 수업. 단축키와 출력

--- Cpp_Project_0515/Cpp_Project_0515.cpp
+++ Cpp_Project_0515/Cpp_Project_0515.cpp
@@ -1,6 +1,12 @@
 ﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "Hello World!1\n";
 }
+
+
---
+++ Cpp_Project_0515/Day0515.h
@@ -0,0 +1,82 @@
+#pragma once
+/*
+언리얼에서는 printf스타일의 출력을 사용하기 때문에 printf를 사용하자
+
+\n, \r
+윈도우에서는 전자만 해도 둘 다 되는 효과
+이외의 운영체제는 둘 다 해줘야 정상 동작
+*/
+
+/*
+
+	cout << "■□■□■□■□■□■□■□■□\n";
+	cout << "↑→↓↓↓\n";
+	cout << "↓←↓→↑←↓\n";
+
+	cout << "\n";
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = i; j < 10; j++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	cout << "\n";
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	cout << "\n";
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			cout << " ";
+		}
+		for (int j = i; j < 10; j++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	for (int i = 10; i >= 0; i--)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			cout << " ";
+		}
+		for (int j = i; j < 10; j++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	cout << "\n";
+
+	int height;
+	cin >> height;
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < height - i - 1; j++)
+			cout << " ";
+		for (int j = 0; j < 2 * i - 1; j++)
+			cout << "*";
+
+		cout << "\n";
+	}
+	return 0;
+*/
+// 
